42c9a2033e6af1dfe6b788ac98f97d497fb2c78f
Joey Yannuzzi <joeygym2000@gmail.com>
2023-04-10 20:06:34 -0400

    milestone 1

---
+++ lib/functions.php
@@ -0,0 +1,13 @@
+<?php
+require_once(__DIR__ . "/db.php");
+$BASE_PATH = '/project/';
+
+//require safer_echo.php
+require(__DIR__ . "/safer_echo.php");
+
+require(__DIR__ . "/sanitizers.php");
+
+require(__DIR__ . "/user_helpers.php");
+
+//TODO 4: Flash Message Helpers
+?>
